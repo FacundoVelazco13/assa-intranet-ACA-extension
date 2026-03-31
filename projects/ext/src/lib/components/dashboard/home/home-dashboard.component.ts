8abe3040eb32a042944d63bfb202ccb56f3de5a5
FacundoVelazco <facundovelazco133@gmail.com>
2026-03-31 15:39:14 -0300

    FIX : Actualización de links en home page

--- projects/ext/src/lib/components/dashboard/home/home-dashboard.component.ts
+++ projects/ext/src/lib/components/dashboard/home/home-dashboard.component.ts
@@ -5,10 +5,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'aca-home-dashboard',
-  imports: [CommonModule, PageLayoutComponent, MatCardModule, MatButtonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, RouterLink, PageLayoutComponent, MatCardModule, MatButtonModule, MatListModule, MatIconModule],
   templateUrl: './home-dashboard.component.html',
   styleUrl: './home-dashboard.component.scss',
   encapsulation: ViewEncapsulation.None
